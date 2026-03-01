bda9f0f47db0454eefc5db280d9acf7f565ea38a
usadev1984 <chadodev84@proton.me>
2026-03-02 01:08:52 +0300

    danbooru: allocate pattern and match data
    Signed-off-by: usadev1984 <chadodev84@proton.me>

--- src/danbooru/danbooru.c
+++ src/danbooru/danbooru.c
@@ -1,8 +1,13 @@
 #include "danbooru.h"
 #include "print.h"
 
+#include <assert.h>
 #include <chlsdl/common/common.h>
 #include <chlsdl/module.h>
+#include <stdlib.h>
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
+
 
 struct module g_libdanbooru = {
     danbooru_deinit,
@@ -12,6 +17,16 @@ const struct module *
 danbooru_init(const struct chlsdl_data * cdata)
 {
     print_debug_warn("initalizing danbooru...\n");
+
+    g_libdanbooru.regex.pattern
+        = pcre2_compile((PCRE2_SPTR8) "https://cdn\\.donmai\\.us/original/"
+                                      ".*\\.(?:png|jpg|jpeg)",
+            PCRE2_ZERO_TERMINATED, 0, &(int) { 0 }, &(PCRE2_SIZE) { 0 }, NULL);
+    assert(g_libdanbooru.regex.pattern);
+    g_libdanbooru.regex.md = pcre2_match_data_create_from_pattern(
+        g_libdanbooru.regex.pattern, NULL);
+    assert(g_libdanbooru.regex.md);
+
     return &g_libdanbooru;
 }
 
@@ -19,4 +34,6 @@ void
 danbooru_deinit()
 {
     print_debug_warn("cleaning up danbooru...\n");
+    free(g_libdanbooru.regex.md);
+    free(g_libdanbooru.regex.pattern);
 }
