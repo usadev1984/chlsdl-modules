02807d429ba452050e242b27ae0c73683735db96
usadev1984 <chadodev84@proton.me>
2026-03-11 13:39:54 +0300

    danbooru: adjust `include`s and build flags after chlsdl-common was moved from main repo
    Signed-off-by: usadev1984 <chadodev84@proton.me>

--- src/danbooru/config.mk
+++ src/danbooru/config.mk
@@ -1,5 +1,6 @@
-LIBS = -lchlsdl-common -lpcre2-8 -ljson-c -lcurl
+LIBS = -L../chlsdl-common -lchlsdl-common -lpcre2-8 -ljson-c -lcurl
 
+INCS = -I../../include
 CFLAGS = -std=gnu23 -fPIC -Wall -Wextra ${INCS}
 
 LDFLAGS = ${LIBS}
