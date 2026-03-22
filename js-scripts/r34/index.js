b31eb2da6fd792c0b0b98648f35b51be476a7371
usadev1984 <chadodev84@proton.me>
2026-03-22 16:03:10 +0300

    r34:js: use `chlsdl_send_to_chlsdl()`
    Signed-off-by: usadev1984 <chadodev84@proton.me>

--- js-scripts/r34/index.js
+++ js-scripts/r34/index.js
@@ -5,26 +5,13 @@
 // @grant       clipboardWrite
 // @grant       GM_setClipboard
 // @grant       GM_xmlhttpRequest
+// @require     https://raw.githubusercontent.com/usadev1984/chlsdl-modules/refs/heads/latest/js-scripts/chlsdl-common/index.js
 // @version     1.0
 // @author      usadev1984
 // @description 3/9/2026, 10:43:14 PM
 // ==/UserScript==
 (function() {
     'use strict';
-
-    let headers = {
-        "User-Agent": "",
-        "Accept": "",
-        "Accept-Language": "",
-        "Accept-Encoding": "",
-        "Content-Type": "",
-        "Content-Length": "",
-        "Sec-GPC": "",
-        "Connection": "",
-        "Sec-Fetch-Dest": "",
-        "Sec-Fetch-Mode": "",
-        "Sec-Fetch-Site": "",
-    };
 
     document.onkeyup = function(e) {
         if (e.which != 192) // `
@@ -53,7 +40,8 @@
         let source = window.location.href.match(/.+(?<=[\?&])id=[0-9]+/)[0];
 
         var postlist = document.querySelectorAll('article[id^="post_"]');
-        let final_data = url + "\n" + JSON.stringify({
+
+        chlsdl_send_to_chlsdl(url, {
             "url": url,
             "name": name,
             "copyright": get_post_tags('copyright'),
@@ -63,31 +51,6 @@
             "meta": get_post_tags('metadata'),
             "source": source,
             "information": get_post_info(),
-        });
-
-        // GM_setClipboard(final_data, "text/plain");
-
-        GM_xmlhttpRequest({
-            url: "http://localhost:53162",
-            method: "POST",
-            anonymouse: true,
-            /**
-             * setting a timeout appears to be useless since `onerror` gets called
-             * instead when we can't connect to the main program because it
-             * didn't have a socket open.
-             * unrelated: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
-             */
-            // timeout: 2000,
-            // ontimeout: (function (x) {
-            //     console.log(x)
-            //     console.error("post request timed out");
-            // }),
-            onerror: (function (x) {
-                console.error("failed to post data. is the main program running?");
-                console.log(x);
-            }),
-            headers: headers,
-            data: final_data,
         });
     }
 
