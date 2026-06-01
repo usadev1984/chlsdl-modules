a71915d296aacea63db8ebdc29aab00323ec425a
usadev1984 <chadodev84@proton.me>
2026-06-01 14:19:31 +0300

    gelbooru: use print-and-notify macros from shared.h
    Signed-off-by: usadev1984 <chadodev84@proton.me>

--- src/gelbooru/gelbooru.c
+++ src/gelbooru/gelbooru.c
@@ -15,34 +15,8 @@
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 
-#ifdef USE_LIBNOTIFY
-#    define MOD_PRINT_AND_NOTIFY(print, fmt, ...)                              \
-        ({                                                                     \
-            char * body = svconcat(fmt __VA_OPT__(, ) __VA_ARGS__);            \
-            assert(body);                                                      \
-            print("%s\n", body);                                               \
-            chlsdl_notify_notification_show_new("chlsdl-gelbooru", body,       \
-                .timeout = default_notification_timeout);                      \
-            free(body);                                                        \
-        })
-
-#    define MOD_ERROR_AND_NOTIFY(print, fmt, ...)                              \
-        ({                                                                     \
-            char * body = svconcat(fmt __VA_OPT__(, ) __VA_ARGS__);            \
-            assert(body);                                                      \
-            print("%s\n", body);                                               \
-            chlsdl_notify_notification_show_new("chlsdl-gelbooru", body,       \
-                .timeout = default_notification_timeout,                       \
-                .urgency = chlsdl_notify_urgency_critical);                    \
-            free(body);                                                        \
-        })
-#else
-#    define MOD_PRINT_AND_NOTIFY(print, fmt, ...)                              \
-        print(fmt "\n" __VA_OPT__(, ) __VA_ARGS__)
-
-#    define MOD_ERROR_AND_NOTIFY(print, fmt, ...)                              \
-        print(fmt "\n" __VA_OPT__(, ) __VA_ARGS__)
-#endif
+#define __MOD_NOTIFY_MOD_NAME "gelbooru"
+#include "../chlsdl-common/shared.h"
 
 typedef struct {
     const char * url;
