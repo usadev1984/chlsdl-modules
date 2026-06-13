1705af1b8596fc702323b279f35b89506bbd9a4b
usadev1984 <chadodev84@proton.me>
2026-06-13 15:43:04 +0300

    danbooru: fix regex not matching for mp4 links
    Signed-off-by: usadev1984 <chadodev84@proton.me>

--- src/danbooru/danbooru.c
+++ src/danbooru/danbooru.c
@@ -198,7 +198,7 @@ danbooru_init(const struct chlsdl_data * cdata)
      */
     g_libdanbooru.regex.pattern = pcre2_compile(
         (PCRE2_SPTR8) "https://cdn\\.donmai\\.us/original/"
-                      ".*\\.(?:png|jpg|jpeg|gif|mp4?download=\d)",
+                      ".*\\.(?:png|jpg|jpeg|gif|mp4\\?download=\\d)",
         PCRE2_ZERO_TERMINATED, 0, &(int) { 0 }, &(PCRE2_SIZE) { 0 }, NULL);
     assert(g_libdanbooru.regex.pattern);
     g_libdanbooru.regex.md = pcre2_match_data_create_from_pattern(
